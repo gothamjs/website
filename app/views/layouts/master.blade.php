4027a1a7abdad71f66cab26cbb60e6dacd2e473d
Jeremie Ges <bonjour@gesjeremie.fr>
2015-06-20 20:29:23 +0300

    fix meta description

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-
+	<meta name="description" content="Coffeescript Framework for back-end developers who hate front-end development" />
 	<title>Gotham | Coffeescript Framework for back-end developers who hate front-end development</title>
 
   <!-- Gotham -->
