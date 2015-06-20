ebf0d1c18bd3f8e0a198736592c9d8f777185551
Jeremie Ges <bonjour@gesjeremie.fr>
2015-06-20 20:14:48 +0300

    typo fix

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -2,8 +2,8 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	
-	<title>Gotham | Coffeescript Framework for backend developers who hates front-end development</title>
+
+	<title>Gotham | Coffeescript Framework for back-end developers who hate front-end development</title>
 
   <!-- Gotham -->
   <link rel="stylesheet" type="text/css" href="/stylesheets/vendor.css">
